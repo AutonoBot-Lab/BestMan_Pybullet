1d9d1e7c6fa1355097c8db78e37daeecdde4532d
starry521 <1280868105@qq.com>
2024-04-01 07:57:45 +0000

    修改Navgation部分的代码结构

--- refactor/Install/install.sh
+++ refactor/Install/install.sh
@@ -11,3 +11,6 @@ echo "export PYTHONPATH=\$PYTHONPATH:$project_dir" >> ~/.bashrc
 
 # 配置生效
 source ~/.bashrc
+
+# 配置x11转发服务
+echo "export DISPLAY=host.docker.internal:0" >> ~/.bashrc
--- refactor/Visualization/__init__.py
+++ refactor/Visualization/__init__.py
@@ -1,2 +1,1 @@
-from .Nav_route_visual import Nav_route_visual
 from .Visualizer import Visualizer
