ea757f712404c78d92e921e6cec87c6ec9d602f3
starry521 <1280868105@qq.com>
2024-04-03 06:52:16 +0000

    更新文件路径以及example

--- Examples/navigation_basic.py
+++ Examples/navigation_basic.py
@@ -11,46 +11,46 @@ from Visualization import Visualizer
 from Motion_Planning.Navigation import AStarPlanner, RRTPlanner
 from Utils import load_config
 
-# load kitchen from three scenarios
-index = 0
-if index == 0:
-    from Env.Kitchen_v0 import Kitchen
-elif index == 1:
-    from Env.Kitchen_v1 import Kitchen
-else:
-    assert False, "index should be 0 or 1"
 
-# load config
-config_path = '/GithubCode/BestMan_Pybullet/refactor/config/navigation_basic.yaml'
+# Load config
+config_path = '../Config/navigation_basic.yaml'
 cfg = load_config(config_path)
+print(cfg)
 
-pb_client = Client(cfg.Client)
-pb_client.enable_vertical_view(cfg.Client.Camera_params)
-pb_visualizer = Visualizer(pb_client)
+# Init client and visualizer
+client = Client(cfg.Client)
+visualizer = Visualizer(client, cfg.Visualizer)
 
-# logID = pb_client.start_record("example_manipulation") # start recording
-bestman = Bestman(pb_client, cfg.Robot)  # load robot
-bestman.get_joint_link_info("arm")  # get info about arm
+# Load scene
+scene_path = '../Asset/Scene/Kitchen.json'
+client.create_scene(scene_path)
+
+# logID = pb_client.start_record("example_manipulation")    # start recording
+# Init robot
+bestman = Bestman(client, cfg)
+visualizer.change_robot_color(bestman.get_base_id(), bestman.get_arm_id(), False)
 
 # load table, bowl, and chair
-table_id = pb_client.load_object(
-    "/GithubCode/BestMan_Pybullet/refactor/Asset/URDF_models/furniture_table_rectangle_high/table.urdf",
+table_id = client.load_object(
+    "../Asset/URDF_models/furniture_table_rectangle_high/table.urdf",
     [1.0, 1.0, 0.0],
     [0.0, 0.0, 0.0],
     1.0,
     "table",
     fixed_base=True,
 )
-bowl_id = pb_client.load_object(
-    "/GithubCode/BestMan_Pybullet/refactor/Asset/URDF_models/utensil_bowl_blue/model.urdf",
+
+bowl_id = client.load_object(
+    "../Asset/URDF_models/utensil_bowl_blue/model.urdf",
     [0.6, 0.6, 0.85],
     [0.0, 0.0, 0.0],
     1.0,
     "bowl",
     nav_obstacle_tag=False,
 )
-chair_id = pb_client.load_object(
-    "/GithubCode/BestMan_Pybullet/refactor/Asset/URDF_models/furniture_chair/model.urdf",
+
+chair_id = client.load_object(
+    "../Asset/URDF_models/furniture_chair/model.urdf",
     [-0.3, 0.8, 0.1],
     [math.pi / 2.0 * 3, 0.0, math.pi / 2.0],
     1.5,
@@ -62,26 +62,27 @@ chair_id = pb_client.load_object(
 # print("obstacles for navigation: {}".format(pb_client.obstacle_navigation_ids))
 
 # get bounding box of objects
-aabb_table = pb_client.get_bounding_box(table_id)
-pb_visualizer.draw_aabb(table_id)
+aabb_table = client.get_bounding_box(table_id)
+visualizer.draw_aabb(table_id)
 print("-" * 20 + "\n" + "aabb_table:{}".format(aabb_table))
 
 # plot line connecting init and goal positions
 target_position = [5.0, 1.0, 0]
-pb_visualizer.draw_line([1, 0, 0], target_position)
+visualizer.draw_line([1, 0, 0], target_position)
 
 # navigate algorithm
 goal_base_pose = Pose(target_position, [0.0, 0.0, math.pi / 2.0])
 nav_planner = AStarPlanner(
     robot_size = bestman.get_robot_size(), 
-    obstacles_bounds = pb_client.get_Nav_obstacles_bounds(), 
+    obstacles_bounds = client.get_Nav_obstacles_bounds(), 
     resolution = 0.05, 
     enable_plot = True
 )
+
 # nav_planner = RRTPlanner(
 #     robot_size = bestman.get_robot_size(), 
-#     obstacles_bounds = pb_client.get_Nav_obstacles_bounds(),
-#     enable_plot = False
+#     obstacles_bounds = client.get_Nav_obstacles_bounds(),
+#     enable_plot = True
 # )
 path = nav_planner.plan(start_pose = bestman.get_base_pose(), goal_pose = goal_base_pose)
 
@@ -91,5 +92,5 @@ bestman.navigate_base(goal_base_pose, path)
 # # end recording
 # pb_client.end_record(logID)
 
-pb_client.wait(1000)
-pb_client.disconnect_pybullet()
+client.wait(1000)
+client.disconnect()
