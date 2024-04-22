a9cb635b99fbef448b2c9fa68bace7addb4b785b
yk <97421210+starry521@users.noreply.github.com>
2024-04-22 23:27:38 +0800

    Update conda_install.sh

--- Install/conda_install.sh
+++ Install/conda_install.sh
@@ -4,11 +4,11 @@
 script_dir=$(dirname "$(readlink -f "$0")")
 project_dir=$(dirname "$script_dir")
 echo "export PYTHONPATH=\$PYTHONPATH:$project_dir" >> ~/.bashrc
+
+# Install shared file
+sudo apt update && sudo apt install libgl1-mesa-glx libglib2.0-0
+
+# Configure x11 forwarding service
+echo "export DISPLAY=host.docker.internal:0" >> ~/.bashrc
+
 source ~/.bashrc
-
-# # Install shared file
-# sudo apt install libgl1-mesa-glx
-# sudo apt install libglib2.0-0
-
-# # Configure x11 forwarding service
-# echo "export DISPLAY=host.docker.internal:0" >> ~/.bashrc
