f0f050577b6671d9e2d759962784e044e665221e
starry521 <1280868105@qq.com>
2024-04-22 03:08:07 +0000

    修改样例结构

--- Examples/grasp_bowl_from_drawer_in_kitchen0.py
+++ Examples/grasp_bowl_from_drawer_in_kitchen0.py
@@ -4,6 +4,7 @@
 @Time        :   2023/09/01 07:47:46
 """
 
+import os
 import pybullet as p
 from Motion_Planning.Robot import Bestman, Pose
 from Env import Client
@@ -18,67 +19,78 @@ config_path = '../Config/grasp_bowl_from_drawer_in_kitchen0.yaml'
 cfg = load_config(config_path)
 print(cfg)
 
-# Init client and visualizer
-client = Client(cfg.Client)
-visualizer = Visualizer(client, cfg.Visualizer)
 
-# Load scene
-scene_path = '../Asset/Scene/Kitchen.json'
-client.create_scene(scene_path)
+def main():
+    
+    # Init client and visualizer
+    client = Client(cfg.Client)
+    visualizer = Visualizer(client, cfg.Visualizer)
 
-# logID = pb_client.start_record("example_manipulation")    # start recording
-# Init robot
-bestman = Bestman(client, cfg)
-visualizer.change_robot_color(bestman.get_base_id(), bestman.get_arm_id(), False)
+    # Load scene
+    scene_path = '../Asset/Scene/Kitchen.json'
+    client.create_scene(scene_path)
 
-# open the drawer
-client.change_object_joint_angle("elementA", 36, 0.4)
+    # logID = pb_client.start_record("example_manipulation")    # start recording
+    # Init robot
+    bestman = Bestman(client, cfg)
+    visualizer.change_robot_color(bestman.get_base_id(), bestman.get_arm_id(), False)
 
-visualizer.draw_aabb_link("elementA", 36)
+    # open the drawer
+    client.change_object_joint_angle("elementA", 36, 0.4)
 
-# navigate to standing position
-standing_pose = Pose([2.85, 2.4, 0], [0.0, 0.0, 0.0])
-# nav_planner = AStarPlanner(
-#     robot_size = bestman.get_robot_size(), 
-#     obstacles_bounds = client.get_Nav_obstacles_bounds(), 
-#     resolution = 0.05, 
-#     enable_plot = False
-# )
-nav_planner = RRTPlanner(
-    robot_size = bestman.get_robot_size(), 
-    obstacles_bounds = client.get_Nav_obstacles_bounds(), 
-    enable_plot=False
-)
-path = nav_planner.plan(bestman.get_current_pose(), standing_pose)
-bestman.navigate_base(standing_pose, path, visualize = True)
+    visualizer.draw_aabb_link("elementA", 36)
 
-ompl_planner = OMPL_Planner(
-    bestman,
-    cfg.Planner
-)
+    # navigate to standing position
+    standing_pose = Pose([2.85, 2.4, 0], [0.0, 0.0, 0.0])
+    # nav_planner = AStarPlanner(
+    #     robot_size = bestman.get_robot_size(), 
+    #     obstacles_bounds = client.get_Nav_obstacles_bounds(), 
+    #     resolution = 0.05, 
+    #     enable_plot = False
+    # )
+    nav_planner = RRTPlanner(
+        robot_size = bestman.get_robot_size(), 
+        obstacles_bounds = client.get_Nav_obstacles_bounds(), 
+        enable_plot=False
+    )
+    path = nav_planner.plan(bestman.get_current_pose(), standing_pose)
+    bestman.navigate_base(standing_pose, path, visualize = True)
 
-# get obstacles info
-ompl_planner.get_obstacles_info()
+    ompl_planner = OMPL_Planner(
+        bestman,
+        cfg.Planner
+    )
 
-# load bowl
-bowl_id = client.load_object(
-    "../Asset/URDF_models/utensil_bowl_blue/model.urdf",
-    [3.6, 2.4, 0.6],
-    [0.0, 0.0, 0.0],
-    1.0,
-    "bowl",
-    False
-)
+    # get obstacles info
+    ompl_planner.get_obstacles_info()
 
-# set target object for grasping
-ompl_planner.set_target(bowl_id)
+    # load bowl
+    bowl_id = client.load_object(
+        "../Asset/URDF_models/utensil_bowl_blue/model.urdf",
+        [3.6, 2.4, 0.6],
+        [0.0, 0.0, 0.0],
+        1.0,
+        "bowl",
+        False
+    )
 
-# reach target object
-ompl_planner.plan_execute()
+    # set target object for grasping
+    ompl_planner.set_target(bowl_id)
 
-# grsip target object
-bestman.active_gripper(bowl_id, 1)
+    # reach target object
+    ompl_planner.plan_execute()
 
-# disconnect pybullet
-client.wait(1000)
-client.disconnect()
+    # grsip target object
+    bestman.active_gripper(bowl_id, 1)
+
+    # disconnect pybullet
+    client.wait(1000)
+    client.disconnect()
+
+
+if __name__=='__main__':
+    
+    # set work dir to Examples
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    
+    main()
