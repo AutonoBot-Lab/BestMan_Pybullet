8c72407c6b8c51a149184b903c4c7601e5d390a4
starry521 <1280868105@qq.com>
2024-04-23 02:42:27 +0000

    更新conda_install.sh

--- Install/conda_install.sh
+++ Install/conda_install.sh
@@ -3,10 +3,13 @@
 # Add this project to the python search path
 script_dir=$(dirname "$(readlink -f "$0")")
 project_dir=$(dirname "$script_dir")
-echo "export PYTHONPATH=\$PYTHONPATH:$project_dir" >> ~/.bashrc
+if [ -z "$PYTHONPATH" ] || ! echo "$PYTHONPATH" | grep -q "$project_dir"; then
+    # If PYTHONPATH doesn't exist, set it to project directory
+    echo "export PYTHONPATH=\$PYTHONPATH:$project_dir" >> ~/.bashrc
+fi
 
 # Install shared file
-sudo apt update && sudo apt install libgl1-mesa-glx libglib2.0-0
+# sudo apt update && sudo apt install libgl1-mesa-glx libglib2.0-0
 
 # Configure x11 forwarding service
 # echo "export DISPLAY=host.docker.internal:0" >> ~/.bashrc
