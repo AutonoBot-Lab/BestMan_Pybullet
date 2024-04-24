731570bfd8198947db6d157274e1695a21d37b19
starry521 <1280868105@qq.com>
2024-04-24 03:24:44 +0000

    上传目标检测算法的其中一种LangSAM

---
+++ CV/Object_detection/utils.py
@@ -0,0 +1,20 @@
+from PIL import ImageDraw
+
+def draw_rectangle(image, bbox, width=5):
+    """
+    draw a bounding box on an image
+    """
+    
+    img_drw = ImageDraw.Draw(image)
+    x1, y1, x2, y2 = bbox[0], bbox[1], bbox[2], bbox[3]
+
+    width_increase = 5
+    for _ in range(width_increase):
+        img_drw.rectangle([(x1, y1), (x2, y2)], outline="green")
+
+        x1 -= 1
+        y1 -= 1
+        x2 += 1
+        y2 += 1
+    
+    return img_drw
