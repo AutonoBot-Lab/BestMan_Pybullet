1363911918a08c98d1f4d7d532ca3c4628aa7291
starry521 <1280868105@qq.com>
2024-06-07 11:08:37 +0800

    Pose类修改为可以接受旋转矩阵与欧拉角两种输入，修改Grasp_Pose_Estimation描述

--- RoboticsToolBox/Pose.py
+++ RoboticsToolBox/Pose.py
@@ -1,3 +1,6 @@
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
 class Pose:
     def __init__(self, position, orientation):
         """
@@ -5,7 +8,8 @@ class Pose:
 
         Parameters:
             position (list): A list of three floats representing the position in 3D space.
-            orientation (list): A list of three floats representing the orientation in 3D space.
+            orientation (list or np.ndarray): A list of three floats representing the orientation in 3D space (Euler angles)
+                                              or a 3x3 rotation matrix.
 
         Attributes:
             position (list): A list of three floats representing the position in 3D space.
@@ -17,8 +21,19 @@ class Pose:
             pitch (float): The pitch component of the orientation.
             yaw (float): The yaw component of the orientation.
         """
+        
         self.position = position
         self.x, self.y, self.z = position
         
-        self.orientation = orientation
-        self.roll, self.pitch, self.yaw = orientation
+        # Automatically detect orientation type and convert
+        if isinstance(orientation, np.ndarray) and orientation.shape == (3, 3):
+            # If it is a 3x3 numpy ndarray, it is assumed to be a rotation matrix
+            r = R.from_matrix(orientation)
+            self.orientation = r.as_euler('xyz', degrees=False)
+        elif isinstance(orientation, (list, np.ndarray)) and len(orientation) == 3:
+            # If it is a list or a one-dimensional numpy ndarray containing three elements, Euler angles are assumed
+            self.orientation = orientation
+        else:
+            raise ValueError("Orientation must be a list of three floats or a 3x3 rotation matrix")
+        
+        self.roll, self.pitch, self.yaw = self.orientation
