7e10f717000e0afbe6020cd6b3b1aa34b0e4aedc
starry521 <1280868105@qq.com>
2024-06-11 09:41:59 -0400

    更新Install目录下的文件

--- Install/pythonpath.sh
+++ Install/pythonpath.sh
@@ -7,9 +7,5 @@ if [ -z "$PYTHONPATH" ] || ! echo "$PYTHONPATH" | grep -q "$project_dir"; then
     # If PYTHONPATH doesn't exist, set it to project directory
     echo "export PYTHONPATH=\$PYTHONPATH:$project_dir" >> ~/.bashrc
 fi
-
-# Install shared file
-sudo apt update && sudo apt install -y libgl1-mesa-glx libglib2.0-0
-sudo mkdir /usr/lib/dri
-cd /usr/lib/dri
-sudo ln -s /lib/x86_64-linux-gnu/dri/swrast_dri.so swrast_dri.so
+# Create conda environment
+mamba env create -f basic_environment.yaml
