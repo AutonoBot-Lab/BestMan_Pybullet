5a1f01da51d9aa071af3044c14a659b9bee15933
starry521 <1280868105@qq.com>
2024-06-24 13:38:19 +0000

    简单修改
    (cherry picked from commit a661673750296580ebebb930c845bfabc3fc4ff1)

--- RoboticsToolBox/Bestman_sim_ur5e_vacuum_long.py
+++ RoboticsToolBox/Bestman_sim_ur5e_vacuum_long.py
@@ -12,7 +12,7 @@ from .Bestman_sim import Bestman_sim
 class Bestman_sim_ur5e_vacuum_long(Bestman_sim):
     
     def __init__(self, client, visualizer,  cfg):
-        """BestMan_sim for panda arm.
+        """Bestman_sim for ur5e arm.
         """
         
         # Init parent class: BestMan_sim
@@ -85,5 +85,46 @@ class Bestman_sim_ur5e_vacuum_long(Bestman_sim):
                     physicsClientId=self.client_id,
                 )
                 print("-" * 20 + "\n" + "Gripper has been activated!")
+        
+        self.client.run(240)
+        
+    def sim_test_active_gripper(self, object, link_id, value):
+        """
+        Activate or deactivate the gripper.
+
+        Args:
+            object_id (init): ID of the object related to gripper action.
+            value (int): 0 or 1, where 0 means deactivate (ungrasp) and 1 means activate (grasp).
+        """
+
+        if isinstance(object, str):
+            if hasattr(self.client, object):
+                object_id = getattr(self.client, object)
+            else:
+                raise AttributeError(f"scene has not {object} object!")
+        else:
+            object_id = object
+
+        if value == 0 and self.gripper_id != None:
+            p.removeConstraint(self.gripper_id, physicsClientId=self.client_id)
+            self.gripper_id = None
+            print("-" * 20 + "\n" + "Gripper has been deactivated!")
+        
+        
+        if value == 1 and self.gripper_id == None:
+            cube_orn = p.getQuaternionFromEuler([0, math.pi, 0])  # control rotation
+            self.gripper_id = p.createConstraint(
+                self.arm_id,
+                self.tcp_link,
+                object_id,
+                link_id,
+                p.JOINT_FIXED,
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                childFrameOrientation=cube_orn,
+                physicsClientId=self.client_id,
+            )
+            print("-" * 20 + "\n" + "Gripper has been activated!")
         
         self.client.run(240)
